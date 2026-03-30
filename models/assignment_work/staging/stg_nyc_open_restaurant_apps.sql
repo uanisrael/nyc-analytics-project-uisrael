a5f912c1527d870f97f85765615b76f89183968d
Uan Israel <uan.israel@baruchmail.cuny.edu>
2026-03-30 01:00:50 +0000

    fixed a lot of the source code

--- models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
+++ models/assignment_work/staging/stg_nyc_open_restaurant_apps.sql
@@ -3,43 +3,31 @@
 
 WITH source AS (
     SELECT *
-    FROM {{ source('raw', 'source_nyc_open_restaurant_apps') }}
+    FROM {{ source('restaurant_raw', 'source_nyc_open_restaurant_apps') }}
 ),
 
 cleaned AS (
     SELECT
-        -- Keep all other columns except ones we are transforming
-        * EXCEPT (
-            objectid,
-            restaurant_name,
-            borough,
-            zip_code,
-            time_of_submission,
-            approved_for_sidewalk_seating,
-            approved_for_roadway_seating
-        ),
+        *,
 
         -- Identifiers
         CAST(objectid AS STRING) AS application_id,
 
-        -- Restaurant info
-        CAST(restaurant_name AS STRING) AS restaurant_name,
-
         -- Location
-        UPPER(TRIM(CAST(borough AS STRING))) AS borough,
-        CAST(zip_code AS STRING) AS zip_code,
+        UPPER(TRIM(CAST(borough AS STRING))) AS borough_clean,
+        NULL AS zip_code_clean,
 
         -- Time
-        CAST(time_of_submission AS TIMESTAMP) AS time_of_submission,
+        CAST(time_of_submission AS TIMESTAMP) AS submission_timestamp,
 
-        -- Convert approvals to clean TRUE/FALSE
+        -- Approval fields
         CASE 
-            WHEN LOWER(approved_for_sidewalk_seating) = 'yes' THEN TRUE
+            WHEN LOWER(CAST(approved_for_sidewalk_seating AS STRING)) = 'yes' THEN TRUE
             ELSE FALSE
         END AS approved_for_sidewalk,
 
         CASE 
-            WHEN LOWER(approved_for_roadway_seating) = 'yes' THEN TRUE
+            WHEN LOWER(CAST(approved_for_roadway_seating AS STRING)) = 'yes' THEN TRUE
             ELSE FALSE
         END AS approved_for_roadway,
 
@@ -47,12 +35,12 @@ cleaned AS (
         CURRENT_TIMESTAMP() AS _stg_loaded_at
 
     FROM source
-
-    -- Basic filter
     WHERE borough IS NOT NULL
 
-    -- Deduplicate
-    QUALIFY ROW_NUMBER() OVER (PARTITION BY objectid ORDER BY time_of_submission DESC) = 1
+    QUALIFY ROW_NUMBER() OVER (
+        PARTITION BY objectid
+        ORDER BY time_of_submission DESC
+    ) = 1
 )
 
 SELECT * FROM cleaned
